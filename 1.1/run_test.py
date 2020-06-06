07e9425326c2d8c66625cf00c46e420e7e5a0ea1
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2020-06-06 17:34:39 +0100

    ENH: Build 1.1

---
+++ 1.1/run_test.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+import subprocess
+import tempfile
+import unittest
+
+TEST_SCRIPT = """\
+import sys
+print sys.version
+"""
+
+TAG = "jvkersch/python-historic:1.1"
+
+
+class TestImage(unittest.TestCase):
+
+    def test_version(self):
+        # Given
+        folder, script = self._write_test_script()
+
+        # When
+        output = self._run_docker_command(
+            ["-it",
+             "--rm",
+             "-v", "{}:/usr/src/testapp".format(folder),
+             "-w", "/usr/src/testapp",
+             TAG,
+             "python",
+             script]
+        )
+
+        # Then
+        self.assertRegex(output, "^1.1")
+
+    def _write_test_script(self):
+        d = tempfile.mkdtemp()
+        self.addCleanup(shutil.rmtree, d)
+
+        fname = os.path.join(d, "script.py")
+        with open(fname, "wt") as fp:
+            fp.write(TEST_SCRIPT)
+
+        return d, "script.py"
+
+    def _run_docker_command(self, params):
+        cmd = ["docker", "run"]
+        proc = subprocess.run(
+            cmd + params,
+            check=True,
+            stdout=subprocess.PIPE,
+            encoding="ascii",
+        )
+
+        return proc.stdout
+
+
+if __name__ == '__main__':
+    unittest.main()
